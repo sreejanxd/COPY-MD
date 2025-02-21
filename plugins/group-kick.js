1621b25a86cc7d415bf123b320ace5065c0d4263
Jawad Tech <187945040+JawadYTX@users.noreply.github.com>
2025-02-21 15:22:11 +0500

    Update and rename gc-kick.js to group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -2,7 +2,6 @@ const config = require('../config')
 const { cmd, commands } = require('../command')
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
 
-
 cmd({
     pattern: "kick",
     react: "🥏",
