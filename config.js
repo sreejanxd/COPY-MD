b7995e9a5a91890b1079073196c865d88153d7ed
Jawad Tech <187945040+JawadYTX@users.noreply.github.com>
2025-02-28 19:37:46 +0500

    Update config.js

--- config.js
+++ config.js
@@ -61,7 +61,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "923427582273",
+DEV: process.env.DEV || "923470027813",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
